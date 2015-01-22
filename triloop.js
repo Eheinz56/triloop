5c1322b25550cad0c33d68775b00217a491e700a
Eric Heinzman <ehu9079@ehu9079-VirtualBox.(none)>
2015-01-21 20:21:49 -0500

    adding triloop to github page

---
+++ triloop.js
@@ -0,0 +1,6 @@
+var number = "#";
+//creating a variable 
+for(var number="#"; number<"#######"; number+="#")
+//for loop will run through till # is greater than 7 #'s
+console.log(number);
+//printing number
