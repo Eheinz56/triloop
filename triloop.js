739c3872e294a1d18d73bc4be774b46403633fd1
Eric Heinzman <ehu9079@ehu9079-VirtualBox.(none)>
2015-01-26 12:11:03 -0500

    Added commentes

--- triloop.js
+++ triloop.js
@@ -1,3 +1,10 @@
+
+//Eric Heinzman
+//Triloop
+//will loop through and print 7 # and decrease by one each time
+//1/26/15
+
+
 var number = "#";
 //creating a variable 
 for(var number="#"; number<"#######"; number+="#")
